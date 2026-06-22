7d525839187d97b3dc049310a929e702a31f2863
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2026-06-22 14:58:55 -0700

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int elementsNotEqualToVal = 0 ;
+
+        
+
+        for(int i=0;i<nums.length;i++) {
+            if (nums[i]!=val) {
+                nums[elementsNotEqualToVal]=nums[i];
+                elementsNotEqualToVal++;
+            }
+
+        }
+
+        return elementsNotEqualToVal;
+    }
+}
