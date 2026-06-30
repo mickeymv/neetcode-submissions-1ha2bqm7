970be138efc8807ae8a56e8361f5311a5ecab991
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2026-06-30 14:59:44 -0700

    Add: isomorphic-strings - submission-2

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        if(s.length()!=t.length()) {
+            return false;
+        }
+        
+        HashMap<Character, Character> sMap = new HashMap<>();
+        HashMap<Character, Character> tMap = new HashMap<>();
+
+        for(int i=0;i<s.length();i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if(sMap.containsKey(sChar)) {
+                if(tChar!=sMap.get(sChar)) {
+                    return false;
+                }
+            } else {
+                sMap.put(sChar, tChar);
+            }
+
+            if(tMap.containsKey(tChar)) {
+                if(sChar!=tMap.get(tChar)) {
+                    return false;
+                }
+            } else {
+                tMap.put(tChar, sChar);
+            }
+        }
+
+        return true;
+    }
+}
